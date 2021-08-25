9354d924172cbc99e3926ecdd661d60856c8959a
Martins-Okounghae <martins.okuonghae@outlook.com>
2021-08-25 13:25:45 +0100

    creating individual components

---
+++ src/Heading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+export default Heading;
